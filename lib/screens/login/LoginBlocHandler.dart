115ce8f6ad18fa8998e247bb6a11d3cb6e4f2433
Noel Daniel T. Seldura <daniel.seldura@gmail.com>
2020-07-03 19:50:54 +0800

    Location stream works on google maps

--- lib/screens/login/LoginBlocHandler.dart
+++ lib/screens/login/LoginBlocHandler.dart
@@ -1,4 +1,5 @@
 import 'package:carspace/model/GlobalData.dart';
+import 'package:carspace/screens/Home/MapScreen.dart';
 import 'package:carspace/screens/login/LandingScreen.dart';
 import 'package:carspace/screens/prompts/LoadingScreen.dart';
 import 'package:carspace/services/AuthService.dart';
@@ -33,7 +34,7 @@ class _LoginBlocHandlerState extends State<LoginBlocHandler> {
             Provider.of<GlobalData>(context, listen: false).user = state.user;
             Navigator.of(context).pushReplacement(
               MaterialPageRoute(
-                builder: (BuildContext context) => HomeScreen(),
+                builder: (BuildContext context) => MapScreen(),
               ),
             );
           }
