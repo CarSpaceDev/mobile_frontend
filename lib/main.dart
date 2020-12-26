c6c706642ef99fea490e97ce1acde254355c58ec
Jay Ann Dela Cerna <jayanndelacerna91@gmail.com>
2020-12-26 10:33:33 +0800

    1

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:carspace/screens/login/LandingScreen.dart';
 import 'package:carspace/screens/registration/number_registration_screen.dart';
 import 'package:carspace/services/ApiService.dart';
 import 'package:carspace/services/AuthService.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
@@ -13,11 +14,13 @@ import 'model/GlobalData.dart';
 
 void main() async {
 
-  WidgetsFlutterBinding.ensureInitialized();
-  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light
-      .copyWith(systemNavigationBarColor: Colors.indigo[900], statusBarColor: Colors.indigo[900]));
-  runApp(CarSpaceApp());
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  // SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light
+  //     .copyWith(systemNavigationBarColor: Colors.indigo[900], statusBarColor: Colors.indigo[900]));
+  // runApp(CarSpaceApp());
+
+  runApp(DevicePreview(builder: (context)=> CarSpaceApp()));
 }
 
 class CarSpaceApp extends StatelessWidget {
