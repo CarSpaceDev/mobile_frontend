2fc1207cd983f7dc976f365cc4caa9cf689c8ed0
Daniel Seldura <daniel.seldura@gmail.com>
2021-05-03 15:23:18 +0800

    very minimal bugfixes and ease of life updates

--- lib/screens/Wallet/WalletScreen.dart
+++ lib/screens/Wallet/WalletScreen.dart
@@ -1,8 +1,10 @@
 import 'package:carspace/constants/GlobalConstants.dart';
 import 'package:carspace/model/CSUser.dart';
+import 'package:carspace/model/Reservation.dart';
 import 'package:carspace/model/Wallet.dart';
 import 'package:carspace/reusable/CSText.dart';
 import 'package:carspace/reusable/CSTile.dart';
+import 'package:carspace/reusable/LotImageWidget.dart';
 import 'package:carspace/reusable/PopupNotifications.dart';
 import 'package:carspace/screens/Wallet/CashInScreen.dart';
 import 'package:carspace/screens/Wallet/CashOutScreen.dart';
@@ -30,9 +32,11 @@ class _WalletScreenState extends State<WalletScreen> {
   @override
   void initState() {
     controller = ScrollController();
-    FirebaseFirestore.instance.collection("users").doc(locator<AuthService>().currentUser().uid).get().then((doc){ setState(() {
-      user = CSUser.fromDoc(doc);
-    });});
+    FirebaseFirestore.instance.collection("users").doc(locator<AuthService>().currentUser().uid).get().then((doc) {
+      setState(() {
+        user = CSUser.fromDoc(doc);
+      });
+    });
     super.initState();
   }
 
@@ -121,42 +125,42 @@ class _WalletScreenState extends State<WalletScreen> {
                         ),
                       ),
                     ),
-                    if((user!=null && user.partnerAccess > 110))
-                    Container(
-                      width: 16,
-                    ),
-                    if((user!=null && user.partnerAccess > 110))
-                    Flexible(
-                      child: CSTile(
-                        onTap: () {
-                          locator<NavigationService>().pushNavigateToWidget(
-                            getPageRoute(
-                              CashOutScreen(),
-                              RouteSettings(name: "CASHOUT"),
-                            ),
-                          );
-                        },
-                        shadow: true,
-                        padding: EdgeInsets.fromLTRB(16, 16, 16, 16),
-                        borderRadius: 15,
-                        margin: EdgeInsets.zero,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(
-                              CupertinoIcons.creditcard,
-                              color: Theme.of(context).primaryColor,
-                            ),
-                            CSText(
-                              "CASH OUT",
-                              textColor: TextColor.Primary,
-                              textType: TextType.Button,
-                              padding: EdgeInsets.only(left: 4, top: 2),
-                            ),
-                          ],
-                        ),
+                    if ((user != null && user.partnerAccess > 110))
+                      Container(
+                        width: 16,
                       ),
-                    )
+                    if ((user != null && user.partnerAccess > 110))
+                      Flexible(
+                        child: CSTile(
+                          onTap: () {
+                            locator<NavigationService>().pushNavigateToWidget(
+                              getPageRoute(
+                                CashOutScreen(),
+                                RouteSettings(name: "CASHOUT"),
+                              ),
+                            );
+                          },
+                          shadow: true,
+                          padding: EdgeInsets.fromLTRB(16, 16, 16, 16),
+                          borderRadius: 15,
+                          margin: EdgeInsets.zero,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                CupertinoIcons.creditcard,
+                                color: Theme.of(context).primaryColor,
+                              ),
+                              CSText(
+                                "CASH OUT",
+                                textColor: TextColor.Primary,
+                                textType: TextType.Button,
+                                padding: EdgeInsets.only(left: 4, top: 2),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
                   ],
                 ),
               ),
@@ -218,42 +222,170 @@ class TransactionRecordDetailWidget extends StatelessWidget {
   TransactionRecordDetailWidget({@required this.transaction});
   @override
   Widget build(BuildContext context) {
-    return CSSegmentedTile(
-      padding: EdgeInsets.all(16),
-      title: CSText(
-        transaction.fromId != locator<AuthService>().currentUser().uid ? transaction.fromName : "YOU",
-        textType: TextType.Button,
-      ),
-      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        CSText(
-          "To: ${transaction.toName}",
-          padding: EdgeInsets.only(top: 4),
-        ),
-        CSText(
-          "Ref: ${transaction.transactionRef}",
-          padding: EdgeInsets.only(top: 4),
-        ),
-        CSText(
-          "${formatDate(transaction.timestamp, [MM, " ", dd, ", ", yyyy, " ", HH, ":", nn])}",
-          padding: EdgeInsets.only(top: 4),
-        ),
-      ]),
-      trailing: CSTile(
-        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-        margin: EdgeInsets.zero,
-        color: transaction.amount.isNegative ? TileColor.Red : TileColor.Green,
-        shadow: true,
-        borderRadius: 5,
-        child: ConstrainedBox(
-          constraints: BoxConstraints(minWidth: 60, maxWidth: 60),
-          child: CSText(
-            transaction.amount.isNegative ? "(${transaction.amount})".replaceAll("-", "") : "${transaction.amount}",
-            textColor: TextColor.White,
-            textType: TextType.H5Bold,
-            textAlign: TextAlign.center,
+    return CSTile(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CSText(
+                    transaction.fromId != locator<AuthService>().currentUser().uid ? transaction.fromName : "YOU",
+                    textType: TextType.Button,
+                  ),
+                  CSText(
+                    "To: ${transaction.toName}",
+                    padding: EdgeInsets.only(top: 4),
+                  ),
+                  CSText(
+                    "${formatDate(transaction.timestamp, [MM, " ", dd, ", ", yyyy, " ", HH, ":", nn])}",
+                    padding: EdgeInsets.only(top: 4),
+                  ),
+                ],
+              ),
+              CSTile(
+                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                margin: EdgeInsets.zero,
+                color: transaction.amount.isNegative ? TileColor.Red : TileColor.Green,
+                shadow: true,
+                borderRadius: 5,
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(minWidth: 60, maxWidth: 60),
+                  child: CSText(
+                    transaction.amount.isNegative
+                        ? "(${transaction.amount})".replaceAll("-", "")
+                        : "${transaction.amount}",
+                    textColor: TextColor.White,
+                    textType: TextType.H5Bold,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ],
           ),
-        ),
+          ReservationInfoWidget(uid: transaction.transactionRef)
+        ],
       ),
+    );
+  }
+}
+
+class ReservationInfoWidget extends StatefulWidget {
+  final String uid;
+  ReservationInfoWidget({@required this.uid});
+  @override
+  _ReservationInfoWidgetState createState() => _ReservationInfoWidgetState();
+}
+
+class _ReservationInfoWidgetState extends State<ReservationInfoWidget> {
+  Reservation reservation;
+  @override
+  void initState() {
+    FirebaseFirestore.instance.collection("reservations").doc(widget.uid).get().then((data) {
+      if (data.exists)
+        setState(() {
+          reservation = Reservation.fromDoc(data);
+        });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (reservation != null)
+      return CSTile(
+        onTap: null,
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+        margin: EdgeInsets.symmetric(horizontal: 0, vertical: 8),
+        shadow: true,
+        borderRadius: 16,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(
+              children: [
+                Flexible(flex: 1, child: LotImageWidget(lotUid: reservation.lotId)),
+                Flexible(
+                  flex: 2,
+                  child: CSText(
+                    reservation.lotAddress,
+                    textType: TextType.H5Bold,
+                    padding: EdgeInsets.only(left: 8),
+                  ),
+                ),
+              ],
+            ),
+            CSTile(
+              margin: EdgeInsets.symmetric(vertical: 8),
+              padding: EdgeInsets.zero,
+              color: TileColor.None,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: RichText(
+                      text: TextSpan(style: TextStyle(color: Colors.black), children: <TextSpan>[
+                        TextSpan(text: 'Date Placed: ', style: TextStyle(color: Colors.grey)),
+                        TextSpan(
+                          text: "${formatDate(reservation.dateCreated, [
+                            MM,
+                            " ",
+                            dd,
+                            ", ",
+                            yyyy,
+                            " ",
+                            h,
+                            ":",
+                            nn
+                          ])} ${reservation.dateCreated.hour < 12 ? "AM" : "PM"}",
+                        )
+                      ]),
+                    ),
+                  ),
+                  if (reservation.dateUpdated != reservation.dateCreated)
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: RichText(
+                        text: TextSpan(style: TextStyle(color: Colors.black), children: <TextSpan>[
+                          TextSpan(text: 'Date Exited: ', style: TextStyle(color: Colors.grey)),
+                          TextSpan(
+                            text: "${formatDate(reservation.dateUpdated, [
+                              MM,
+                              " ",
+                              dd,
+                              ", ",
+                              yyyy,
+                              " ",
+                              h,
+                              ":",
+                              nn
+                            ])} ${reservation.dateUpdated.hour < 12 ? "AM" : "PM"}",
+                          )
+                        ]),
+                      ),
+                    ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: RichText(
+                      text: TextSpan(style: TextStyle(color: Colors.black), children: <TextSpan>[
+                        TextSpan(text: 'Vehicle: ', style: TextStyle(color: Colors.grey)),
+                        TextSpan(text: reservation.vehicleId)
+                      ]),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+    return CSText(
+      "Ref: ${widget.uid}",
+      padding: EdgeInsets.only(top: 4),
     );
   }
 }
